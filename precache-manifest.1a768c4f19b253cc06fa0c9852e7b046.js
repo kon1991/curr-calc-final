57d626d3c4a4d604c263a447ef75c1252cf3c818
Konstantis Dalakouras <codegorilla@hotmail.com>
2019-11-30 10:22:58 +0200

    Updates

---
+++ precache-manifest.1a768c4f19b253cc06fa0c9852e7b046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533ec7762775bda4f98fbe3882cb07e1",
+    "url": "/curr-calc-final/index.html"
+  },
+  {
+    "revision": "e029f3ae4164179f36ae",
+    "url": "/curr-calc-final/static/css/main.40696a29.chunk.css"
+  },
+  {
+    "revision": "847130294d91b737eb7b",
+    "url": "/curr-calc-final/static/js/2.3caf16c8.chunk.js"
+  },
+  {
+    "revision": "e029f3ae4164179f36ae",
+    "url": "/curr-calc-final/static/js/main.98d7468d.chunk.js"
+  },
+  {
+    "revision": "837e2929aac70789f540",
+    "url": "/curr-calc-final/static/js/runtime-main.cd375e0f.js"
+  }
+]);
